c2d4a6b8c5cfd34e520cbd527f6d1d9d0f8f73e5
martincastroc <castrom927@gmail.com>
2023-04-29 18:25:57 -0400

    Added keybindings

--- lua/custom/remaps.lua
+++ lua/custom/remaps.lua
@@ -1,2 +1,17 @@
-vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.g.mapleader = ' ' -- Space as leader key
+
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex) -- Open Netrw
+
+-- Keep cursor in the middle of the screen when jumping half-pages
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("x", "<leader>p", [["_dP]]) -- Dont lose initial yank when pasting
+
+-- Yank into system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Move selected lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
